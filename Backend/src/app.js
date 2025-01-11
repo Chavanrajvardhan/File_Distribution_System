8c08a9962f32341f773ce94957a48effd744ed2a
raj chavan <chavanraj1010@gmail.com>
2025-01-11 14:49:55 +0530

    cut import line

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,6 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import './utils/fileAvailabilityScheduler.js'
 const app = express();
 app.use(
   cors({
