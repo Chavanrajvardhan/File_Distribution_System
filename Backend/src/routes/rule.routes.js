4f4f2e8900a22de7366ab1c15d95daa18bea5e0b
raj chavan <chavanraj1010@gmail.com>
2025-01-22 11:15:41 +0530

    add share file route

--- Backend/src/routes/rule.routes.js
+++ Backend/src/routes/rule.routes.js
@@ -1,15 +1,10 @@
 import { Router } from 'express';
 
 import {
-    shareWithTimeBoundRule,
-<<<<<<< HEAD
-    scheduleFilesToShare,
+    shareFilesWithTimeBound,
     newUserRule,
     shareFilesUsingRules,
     getAllUserRules
-=======
-    scheduleFilesToShare
->>>>>>> 2da7fb94a51e7ba7bb3c14119c4d515f8457a95a
 } from "../controllers/rule.controller.js"
 
 import { verifyJWT } from "../middlwares/auth.middleware.js";
@@ -17,13 +12,10 @@ import { verifyJWT } from "../middlwares/auth.middleware.js";
 const router = Router()
 router.use(verifyJWT); // applay verify jwt for all routes
 
-router.route("/withTimeBound").post(shareWithTimeBoundRule)
-router.route("/scheduleFiles").post(scheduleFilesToShare)
-<<<<<<< HEAD
+router.route("/withTimeBound").post(shareFilesWithTimeBound)
+// router.route("/scheduleFiles").post(scheduleFilesToShare)
 router.route("/AddNewRule").post(newUserRule)
 router.route("/shareFilesUsingRules").post(shareFilesUsingRules)
 router.route("/allUserRules").get(getAllUserRules)
-=======
->>>>>>> 2da7fb94a51e7ba7bb3c14119c4d515f8457a95a
 
 export default router;
