bd38bff7dc4bd48f1e1f8e06f2c8165b1f2fe1e5
Rushikesh Suryawansh <suryawanshir102@gmail.com>
2025-01-30 10:41:01 +0530

    packages add

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
+      "/api": "http://localhost:8000",
     },
   },
   plugins: [react()],
-})
+});
