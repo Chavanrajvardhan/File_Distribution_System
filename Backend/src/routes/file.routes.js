f91356cc39a2527ecec0c39816ac27f6f1706b9a
Rushikesh Suryawansh <suryawanshir102@gmail.com>
2025-01-30 10:25:41 +0530

    comments removed

--- Backend/src/routes/file.routes.js
+++ Backend/src/routes/file.routes.js
@@ -1,43 +1,37 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 import {
-    uploadFiles,
-    getAllUserFiles,
-    getAllReceivers,
-    getAllUserFilesToDownload,
-    deleteFile,
-    getAllDeletedFiles,
-    RestoreFile,
-    permanentDeleteFile,
-    getFileStatus,
+  uploadFiles,
+  getAllUserFiles,
+  getAllReceivers,
+  getAllUserFilesToDownload,
+  deleteFile,
+  getAllDeletedFiles,
+  RestoreFile,
+  permanentDeleteFile,
+  getFileStatus,
 
-    // getFileStatus,
-} from "../controllers/file.controller.js"
+  // getFileStatus,
+} from "../controllers/file.controller.js";
 
 import { verifyJWT } from "../middlwares/auth.middleware.js";
-import {upload} from "../middlwares/multer.middleware.js"
+import { upload } from "../middlwares/multer.middleware.js";
 
-const router = Router()
+const router = Router();
 router.use(verifyJWT); // applay verify jwt for all routes
 
 router.route("/uploadFile").post(
-    upload.array("file", 25), // Accept up to 25 files for the "file" field
-    uploadFiles
+  upload.array("file", 25), // Accept up to 25 files for the "file" field
+  uploadFiles
 );
 
-router.route("/allUserFiles/:userId").post(getAllUserFiles)
-router.route("/getAllReceivers").get(getAllReceivers)
+router.route("/allUserFiles/:userId").post(getAllUserFiles);
+router.route("/getAllReceivers").get(getAllReceivers);
 router.route("/availableToDownload").post(getAllUserFilesToDownload);
 router.route("/deleteFile/:fileIds").post(deleteFile);
 router.route("/RestoreFile/:fileId").post(RestoreFile);
 router.route("/permanentDelete/:fileId").delete(permanentDeleteFile);
 router.route("/allDeletedFiles/:userId").get(getAllDeletedFiles);
 router.route("/fileStatus").get(getFileStatus);
-
-
-
-// router.route("/filestats").get(getFileStatus);
-
-
 
 export default router;
