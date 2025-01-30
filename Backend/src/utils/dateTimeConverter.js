e001d0b6f76478522136fe807567636b6f8b30d2
Rushikesh Suryawansh <suryawanshir102@gmail.com>
2025-01-30 10:28:04 +0530

    comments removed

--- Backend/src/utils/dateTimeConverter.js
+++ Backend/src/utils/dateTimeConverter.js
@@ -1,11 +1,15 @@
 export function convertToIndianTime(isoDate) {
-    // Create a new Date object from the ISO datetime string
+    // Check if isoDate is valid
+    if (!isoDate) return null; // Return null for invalid or missing dates
     const date = new Date(isoDate);
-
+ 
+    // Check if the Date object is valid
+    if (isNaN(date.getTime())) {
+      return null; // Return null for invalid date strings
+    }
     // Convert the Date object to IST (Indian Standard Time)
     const indiaTime = date.toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
-
     return indiaTime;
-}
-
-
+  }
+ 
+ 
