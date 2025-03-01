7ea2435197db548a3054a59aaef6bae016ab6cf6
rushikeshs102 <suryawanshir102@gmail.com>
2025-03-01 11:06:18 +0530

    route for auth verification added

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -12,6 +12,12 @@ const router = Router()
 
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
+router.route("/validate").get(verifyJWT, (req, res) => {
+    return res.status(200).json({
+        success: true,
+        data: req.user
+    });
+});
 
 //secured route 
 
